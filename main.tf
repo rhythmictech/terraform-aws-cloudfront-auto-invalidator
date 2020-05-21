709dc4b349c0e7d09cbeb5b9ea98fe6e8ca6db12
Scott Miller <scott@millergeek.xyz>
2020-05-21 15:37:57 -0400

    changed naming as reccomended in review

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_iam_policy_document" "assume" {
   }
 }
 
-resource "aws_iam_role" "lambda" {
+resource "aws_iam_role" "this" {
   name_prefix        = "${var.name}-role-"
   assume_role_policy = data.aws_iam_policy_document.assume.json
 
@@ -30,12 +30,12 @@ resource "aws_iam_role" "lambda" {
 }
 
 resource "aws_iam_role_policy_attachment" "basic_execution" {
-  role       = aws_iam_role.lambda.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "cloudfront_read" {
-  role       = aws_iam_role.lambda.name
+  role       = aws_iam_role.this.name
   policy_arn = "arn:aws:iam::aws:policy/CloudFrontReadOnlyAccess"
 }
 
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "cloudfront_invalidations" {
 resource "aws_iam_role_policy" "cloudfront_invalidations" {
   name_prefix = "cloudfront-invalidations-"
   policy      = data.aws_iam_policy_document.cloudfront_invalidations.json
-  role        = aws_iam_role.lambda.name
+  role        = aws_iam_role.this.name
 }
 
 resource "aws_lambda_function" "this" {
@@ -62,7 +62,7 @@ resource "aws_lambda_function" "this" {
   function_name    = var.name
   handler          = "app.lambda_handler"
   memory_size      = var.memory_size
-  role             = aws_iam_role.lambda.arn
+  role             = aws_iam_role.this.arn
   runtime          = var.runtime
   source_code_hash = data.archive_file.lambda.output_base64sha256
   timeout          = var.timeout
@@ -75,7 +75,7 @@ resource "aws_lambda_function" "this" {
   )
 }
 
-resource "aws_lambda_permission" "allow_S3_notification" {
+resource "aws_lambda_permission" "this" {
   statement_id  = "AllowExecutionFromS3Bucket"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.arn
